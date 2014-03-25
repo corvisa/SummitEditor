6f6b8ef0892a18410c62d3e5c0e41818966289d7
TJ Kells <tj.kells@corvisa.com>
2014-03-25 12:08:42 -0500

    Now with working simulator build

--- simulate.sh
+++ simulate.sh
@@ -1,9 +1,14 @@
 
-#!/usr/bin/env bash
-path="$1"
-if [ -e "$path"/main.lua ]; then path=`dirname "$path"`; fi
-token=`/bin/tar -cz --exclude .git -C "$path" .|ssh -q summitdebug@lunaapp1.dev1.int.corvisacloud.com`
-ssh summitdebug@lunaapp1.dev1.int.corvisacloud.com $token
+#!/bin/bash
+# path="$1"
+# if [ -e "$path"/main.lua ]; then path=`dirname "$path"`; fi
+
+token=`/bin/tar -cz --exclude .git -C "/home/tkells/dev/summit_apps/tjtest/" .|ssh -q summitdebug@lunaapp1.dev1.int.corvisacloud.com`
+
+echo $token
+
+/usr/bin/xterm -e /bin/bash -c "ssh -t summitdebug@lunaapp1.dev1.int.corvisacloud.com $token; echo; echo 'press any key to continue'; read -n 1" &
+
 echo
 echo '------ COMPLETE ------'
 read pause
