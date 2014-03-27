98567bb012803ba468fda08e5c9d48eae0169d34
TJ Kells <tj.kells@corvisa.com>
2014-03-27 18:17:24 -0500

    Remove some leftover cruft from testing

--- simulate.sh
+++ simulate.sh
@@ -5,4 +5,4 @@ echo "Deploying project to test server..."
 token=`/bin/tar -cz --exclude .git -C $path .|ssh -q summitdebug@lunaapp1.dev1.int.corvisacloud.com`
 
 echo "Running simulator..."
-/usr/bin/xterm -e /bin/bash -c "ssh -t summitdebug@lunaapp1.dev1.int.corvisacloud.com $token ${*:2}; echo; echo 'Press any key to continue'; read -n 1" &
+/usr/bin/xterm -e /bin/bash -c "ssh -t summitdebug@lunaapp1.dev1.int.corvisacloud.com '$token ${*:2}'; echo; echo 'Press any key to continue'; read -n 1" &
