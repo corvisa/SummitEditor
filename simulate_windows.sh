a9fad10a446a03148d670f8348835bfd9f011237
TJ Kells <tjkells@gmail.com>
2014-03-28 16:24:41 -0500

    Remove all hardcoded paths for things

--- simulate_windows.sh
+++ simulate_windows.sh
@@ -1,13 +1,16 @@
-project_path="$1"
 export PATH=$PATH:"/c/Program Files (x86)/Git/bin":
 
+keyfile="$HOMEDRIVE$HOMEPATH\\.ssh\\id_rsa"
+project_path="$1"
+
 echo "Compressing project..."
-token=`tar -cz --exclude .git -C $project_path .|ssh -i /c/Users/tj.kells/.ssh/id_rsa -q summitdebug@lunaapp1.dev1.int.corvisacloud.com`
+token=`tar -cz --exclude .git -C $project_path .|ssh -i "$keyfile" -q summitdebug@lunaapp1.dev1.int.corvisacloud.com`
+echo "Deploying project to test server..."
+
 clear
 
-echo "Deploying project to test server..."
 echo "Running simulator..."
 
-ssh -t -t -i /c/Users/tj.kells/.ssh/id_rsa summitdebug@lunaapp1.dev1.int.corvisacloud.com $token ${@:2}
+ssh -t -t -i "$keyfile" summitdebug@lunaapp1.dev1.int.corvisacloud.com $token ${@:2}
 echo 'Press any key to continue'
 read -n 1
