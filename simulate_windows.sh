21c628422ecbfdca5c9f7099ee266dd475a11684
TJ Kells <tjkells@gmail.com>
2014-03-28 14:33:09 -0500

    Input now being passed correctly via mingw bash

--- simulate_windows.sh
+++ simulate_windows.sh
@@ -1,14 +1,13 @@
-#!bash
 project_path="$1"
-
-
 export PATH=$PATH:"/c/Program Files (x86)/Git/bin":
+
 echo "Compressing project..."
 token=`tar -cz --exclude .git -C $project_path .|ssh -i /c/Users/tj.kells/.ssh/id_rsa -q summitdebug@lunaapp1.dev1.int.corvisacloud.com`
 clear
+
 echo "Deploying project to test server..."
 echo "Running simulator..."
 
-bash -c "ssh -i /c/Users/tj.kells/.ssh/id_rsa -t summitdebug@lunaapp1.dev1.int.corvisacloud.com "$token ${@:2}"; echo; echo 'Press any key to continue'; read -n 1" &
-
-"C:\Program Files (x86)\Git\bin\sleep" 20
+ssh -t -t -i /c/Users/tj.kells/.ssh/id_rsa summitdebug@lunaapp1.dev1.int.corvisacloud.com $token ${@:2}
+echo 'Press any key to continue'
+read -n 1
