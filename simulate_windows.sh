98e60d9e267b7d7a25bdaad4927eeedb5dc5273a
TJ Kells <tjkells@gmail.com>
2014-03-28 14:12:56 -0500

    First semi-working windows build script

--- simulate_windows.sh
+++ simulate_windows.sh
@@ -1,9 +1,14 @@
-#!/bin/bash
-path="$1"
-shift
+#!bash
+project_path="$1"
 
+
+export PATH=$PATH:"/c/Program Files (x86)/Git/bin":
+echo "Compressing project..."
+token=`tar -cz --exclude .git -C $project_path .|ssh -i /c/Users/tj.kells/.ssh/id_rsa -q summitdebug@lunaapp1.dev1.int.corvisacloud.com`
+clear
 echo "Deploying project to test server..."
-token=`/bin/tar -cz --exclude .git -C $path .|ssh -q summitdebug@lunaapp1.dev1.int.corvisacloud.com`
-
 echo "Running simulator..."
-bash -c "ssh -t summitdebug@lunaapp1.dev1.int.corvisacloud.com $token "$@"; echo; echo 'Press any key to continue'; read -n 1" &
+
+bash -c "ssh -i /c/Users/tj.kells/.ssh/id_rsa -t summitdebug@lunaapp1.dev1.int.corvisacloud.com "$token ${@:2}"; echo; echo 'Press any key to continue'; read -n 1" &
+
+"C:\Program Files (x86)\Git\bin\sleep" 20
