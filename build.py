9f1f48fa56ff25318eb1449ec0c8671782227c97
TJ Kells <tj.kells@corvisa.com>
2014-04-11 14:17:17 -0500

    Refactor build into less obtuse single method

--- build.py
+++ build.py
@@ -18,44 +18,19 @@ class SummitBuild(sublime_plugin.WindowCommand):
             sublime.error_message('Unable to find required project setting "build_path"')
         self.platform = platform.system().lower()
 
-        self._run()
+        self.build()
 
-    def _run(self):
-        try:
-            getattr(self, self.platform+'_build')()
-        except AttributeError:
+    def build(self, opts=[]):
+        if self.platform not in ['windows', 'darwin', 'linux']:
             sublime.error_message(
                 "Unable to determine operating system or platform '{0}' not supported.".format(self.platform))
+            return
 
-    def windows_build(self, opts=[]):
-        cmd = [
-            'bash.exe',
-            "{0}\simulate_windows.sh".format(SUMMIT_PLUGIN_PATH),
-            self.build_path,
-            SUMMIT_SETTINGS.get("summit_simulator_host")
-        ]
-        for opt in opts:
-            cmd.append(opt)
-        Popen(cmd)
-
-    def linux_build(self, opts=[]):
         cmd = [
             'bash',
-            "{0}/simulate.sh".format(SUMMIT_PLUGIN_PATH),
+            "{}{}simulate_{}.sh".format(SUMMIT_PLUGIN_PATH, os.path.sep, self.platform),
             self.build_path,
             SUMMIT_SETTINGS.get("summit_simulator_host")
-        ]
-
-        for opt in opts:
-            cmd.append(opt)
-        Popen(cmd)
-
-    def darwin_build(self, opts=[]):
-        cmd = [
-            'bash',
-            "{0}/simulate_mac.sh".format(SUMMIT_PLUGIN_PATH),
-            self.build_path,
-            SUMMIT_SETTINGS.get("summit_simulator_host"),
         ]
         for opt in opts:
             cmd.append(opt)
@@ -63,12 +38,14 @@ class SummitBuild(sublime_plugin.WindowCommand):
 
 
 class SummitBuildWithArgs(SummitBuild):
-    def _run(self, *args, **kwargs):
+    def run(self, *args, **kwargs):
+        try:
+            self.build_path = self.window.project_data()['build_path']
+        except KeyError:
+            sublime.error_message('Unable to find required project setting "build_path"')
+        self.platform = platform.system().lower()
+
         self.window.show_input_panel("Simulator Arguments (--Arg Value):", "--DNIS ", self.simulate_with_args, None, None)
 
     def simulate_with_args(self, sim_args):
-        try:
-            getattr(self, self.platform+'_build')(sim_args.split())
-        except AttributeError:
-            sublime.error_message(
-                "Unable to determine operating system or platform '{0}' not supported.".format(self.platform))
+        self.build(sim_args.split())
