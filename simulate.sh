8757ae0c6c87ce5b98e27b3c3e818024daa227f3
TJ Kells <tj.kells@corvisa.com>
2014-04-11 11:35:31 -0500

    Make simulator host a configurable option

--- simulate.sh
+++ simulate.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 path="$1"
+simulator_host="$2"
+build_args=${*:3}
 
-echo "Deploying project to test server..."
-token=`/bin/tar -cz --exclude .git -C $path .|ssh -q summitdebug@lunaapp1.dev1.int.corvisacloud.com`
-
-echo "Running simulator..."
-/usr/bin/xterm -e /bin/bash -c "ssh -t summitdebug@lunaapp1.dev1.int.corvisacloud.com '$token ${*:2}'; echo; echo 'Press any key to continue'; read -n 1" &
+token=`/bin/tar -cz --exclude .git -C $path .|ssh -q summitdebug@$simulator_host`
+/usr/bin/xterm -geometry 120x50 -e /bin/bash -c "echo 'Running simulator...'; ssh -t summitdebug@$simulator_host '$token $build_args'; echo; echo 'Press any key to continue'; read -n 1" &
--- simulate_windows.sh
+++ simulate_windows.sh
@@ -2,15 +2,17 @@ export PATH=$PATH:"/c/Program Files (x86)/Git/bin":
 
 keyfile="$HOMEDRIVE$HOMEPATH\\.ssh\\id_rsa"
 project_path="$1"
+simulator_host="$2"
+build_args=${*:3}
 
 echo "Compressing project..."
-token=`tar -cz --exclude .git -C $project_path .|ssh -i "$keyfile" -q summitdebug@lunaapp1.dev1.int.corvisacloud.com`
+token=`tar -cz --exclude .git -C $project_path .|ssh -i "$keyfile" -q summitdebug@$simulator_host`
 echo "Deploying project to test server..."
 
 clear
 
 echo "Running simulator..."
 
-ssh -t -t -i "$keyfile" summitdebug@lunaapp1.dev1.int.corvisacloud.com $token ${@:2}
+ssh -t -t -i "$keyfile" summitdebug@$simulator_host $token $build_args
 echo 'Press any key to continue'
 read -n 1
