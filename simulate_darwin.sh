728fd74d64b103e9e7ccc7dadbd1ee36f1678ead
TJ Kells <tj.kells@corvisa.com>
2014-04-16 13:38:25 -0500

    Fix username in mac build script

--- simulate_darwin.sh
+++ simulate_darwin.sh
@@ -34,7 +34,7 @@ osascript <<END
 	tell application "Terminal"
 	    tell window 1
 	    	activate
-	        set w to do script "clear; bash -c \"ssh -t summitdebug@$simulator_host '$token $build_args'; echo; echo Press any key to continue; read -n 1; exit\""
+	        set w to do script "clear; bash -c \"ssh -t debug@$simulator_host '$token $build_args'; echo; echo Press any key to continue; read -n 1; exit\""
 	    end tell
     end tell
 END
