a4aa55e2c5e84ee6366624dd8b23634c1f436c9f
Anderson, Chase <Chase.Anderson@corvisa.com>
2014-06-18 14:10:03 -0500

    Fixed Package Control install warning to only show on run and not initialization

--- linter_install.py
+++ linter_install.py
@@ -1,12 +1,16 @@
 import sublime, sublime_plugin
 import threading
+import_error = False
 try:
 	package_manager = __import__("Package Control").package_control.package_manager
 except ImportError:
-	sublime.error_message("Package Control is not installed. Install Package Control before continuing.")
+	import_error = True
 
 class LinterInstallCommand(sublime_plugin.ApplicationCommand):
 	def run(self):
+		if import_error:
+			sublime.error_message("Package Control is not installed. Install Package Control before continuing.")
+			return
 		t = threading.Thread(target=self.installer)
 		t.start()
 
